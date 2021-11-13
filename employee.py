e69155370aa06191c3692d59742ce3e290a0075c
suryasengautam <94212580+suryasengautam@users.noreply.github.com>
2021-11-13 15:53:03 +0530

    Update employee.py

--- employee.py
+++ employee.py
@@ -1,26 +1,26 @@
-# ## employee details
-# a=['name','age','salary','designation']
-# b=[['manoj','bipin','nikhil','suraj'],[22,20,21,23],[28000,29000,30000,32000],['noida','mumbai','indor','switzerland']]
-# c=['emp1','emp2','emp3','emp4']
-# n = len(a)
-# dic = {}
-# for i in range(n):
-#    dic1={}
-#    for j in range(n):
-#       dic1[a[j]]=b[j][i]
-#    dic[c[i]]=dic1
-# print(dic)
-# ######################################################################333
-# name=['amol','surya_sen','bhopu']
-# info=['name','destination','salary']
-# info1 = [['amol','surya_sen','bhopu'],['manger','bank_manager','governor'],[2000,500,600]]
-# salary=[2000,500,600]
-# detin=['manger','bank_manager','governor']
-# dic1={}
-# n = len(name)
-# for i in range(n):
-#     dic={}
-#     for j in range(n):
-#         dic[info[j]]=info1[j][i]
-#     dic1[name[i]]=dic
-# print(dic1)
+## employee details
+a=['name','age','salary','designation']
+b=[['manoj','bipin','nikhil','suraj'],[22,20,21,23],[28000,29000,30000,32000],['noida','mumbai','indor','switzerland']]
+c=['emp1','emp2','emp3','emp4']
+n = len(a)
+dic = {}
+for i in range(n):
+   dic1={}
+   for j in range(n):
+      dic1[a[j]]=b[j][i]
+   dic[c[i]]=dic1
+print(dic)
+######################################################################333
+name=['amol','surya_sen','bhopu']
+info=['name','destination','salary']
+info1 = [['amol','surya_sen','bhopu'],['manger','bank_manager','governor'],[2000,500,600]]
+salary=[2000,500,600]
+detin=['manger','bank_manager','governor']
+dic1={}
+n = len(name)
+for i in range(n):
+    dic={}
+    for j in range(n):
+        dic[info[j]]=info1[j][i]
+    dic1[name[i]]=dic
+print(dic1)
